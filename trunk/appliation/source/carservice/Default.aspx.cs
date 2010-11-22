821c6f81d0076ab9b05adea112529fd8a732f2f4
devilfighter1806 <devilfighter1806@bc777496-d745-1a5a-1225-020a83ad18bf>
2010-11-22 22:00:54 +0000

    Initial project structure in VS2010. Created not final entity data model.

---
+++ trunk/appliation/source/carservice/Default.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using persistence;
+using System.Web.Security;
+using System.Text;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (IsPostBack == false)
+        {
+            StringBuilder sb = new StringBuilder();
+            MembershipUserCollection users = Membership.GetAllUsers();
+            foreach (MembershipUser user in users)
+            {
+                sb.Append(user.UserName + ", " + user.Email + "<br/>");
+            }
+            this.users.Text = sb.ToString();
+        }
+    }
+}
