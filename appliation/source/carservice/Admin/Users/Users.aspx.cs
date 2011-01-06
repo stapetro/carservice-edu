8d743d91953946abaf6f32d9aa96f735425c1ee1
devilfighter1806 <devilfighter1806@bc777496-d745-1a5a-1225-020a83ad18bf>
2011-01-06 22:59:11 +0000

    User management implementing...

--- appliation/source/carservice/Admin/Users/Users.aspx.cs
+++ appliation/source/carservice/Admin/Users/Users.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
+using businesslogic;
 
 namespace presentation
 {
@@ -11,7 +13,40 @@ namespace presentation
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            List<CarServiceUser> carServiceUsers = GetUsers();
+            this.carServiceUsers.DataSource = carServiceUsers;
+            this.carServiceUsers.DataBind();
+        }
 
+        private List<CarServiceUser> GetUsers()
+        {
+            List<CarServiceUser> carServiceUsers = new List<CarServiceUser>();
+            MembershipUserCollection membershipUsers = Membership.GetAllUsers();
+            foreach (MembershipUser currentUser in membershipUsers)
+            {
+                ProfileCommon profile = Profile.GetProfile(currentUser.UserName);
+                if (profile != null)
+                {                    
+                    carServiceUsers.Add(new CarServiceUser(currentUser, profile));
+                }
+            }
+            return carServiceUsers;
+        }
+
+        protected void CarServiceUsersGridView_RowCreated(Object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                object isApprovedObject = DataBinder.Eval(e.Row.DataItem, "IsActive");
+                if(isApprovedObject != null)
+                {
+                    bool isApproved = (bool)isApprovedObject;
+                    if (isApproved == false)
+                    {
+                        e.Row.CssClass = "inactive";
+                    }
+                }
+            }
         }
     }
 }
